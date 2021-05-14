91c83ff6dfb6b70858e4606017a554c4d8ca0064
Jael Oseguera <jaelosegueradavid@gmail.com>
2021-05-13 20:20:28 -0600

    EXAMEN1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+//APP
+
 import './App.css';
+import Alumno from './Componente/Alumno';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Alumno        
+        identidad="0908-2000-12345" 
+        nombre="Oscar Antonio Perez Lopez" 
+        nacimiento= "25-may-2000" 
+        promedio={(100+72+94)/3}/>
+        <Alumno
+        identidad="0801-2001-12345" 
+        nombre="Maria Fernanda Juarez" 
+        nacimiento= "12-feb-2001" 
+        promedio={(99+62+71)/3}/>
+        <Alumno
+        identidad="0908-2000-12345" 
+        nombre="Carlos Herminio Aguirre" 
+        nacimiento= "25-may-2000" 
+        promedio={(89+92+74)/3}/>
       </header>
     </div>
   );
 }
 
+
 export default App;
+
